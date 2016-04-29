fa4e0dfb4c51be25b931c4e6b70f7bfdd4532600
wgrand <wgrand@grandinteractive.com>
2016-04-29 17:05:09 -0400

    #1 Basic GameManager set up with unit tests

---
+++ app/src/main/java/com/williamgrand/tictactoe/GameManager.java
@@ -0,0 +1,148 @@
+package com.williamgrand.tictactoe;
+
+/**
+ * Created by wgrand on 4/29/16.
+ */
+public class GameManager {
+
+    public static Player[][] board;
+    private static int boardSize;
+
+    public static void createGame()
+    {
+        // create a game of size 4x4
+        createGame(4);
+    }
+    public static void createGame(int boardSize)
+    {
+        board = new Player[boardSize][boardSize];
+        GameManager.boardSize = boardSize;
+    }
+
+    // region Check Win
+
+    public static boolean checkWin(Player p)
+    {
+        if (checkVerticalWin(p))
+            return true;
+        else if (checkHorizontalWin(p))
+            return true;
+        else if (checkDiagonalWin(p))
+            return true;
+        else if(checkFourCornersWin(p))
+            return true;
+        return false;
+    }
+
+    public static boolean checkVerticalWin(Player p)
+    {
+        // traverse horizontally until we find a player
+        boolean foundWin = false;
+        for (int col = 0; col < boardSize; col++)
+            if (board[col][0] == p) // found possible win, traverse downward to verify
+            {
+                for (int row = 0; row < boardSize; row++)
+                    if (board[col][row] != p)
+                        break; // found another player, continue to loop through the columns
+                foundWin = true;
+            }
+
+        return foundWin;
+
+    }
+
+    public static boolean checkHorizontalWin(Player p)
+    {
+        // traverse vertically until we find a player
+        boolean foundWin = false;
+        for (int row = 0; row < boardSize; row++)
+            if (board[0][row] == p) // found possible win, traverse downward to verify
+            {
+                for (int col = 0; col < boardSize; col++)
+                    if (board[col][row] != p)
+                        break; // found another player, continue to loop through the rows
+                foundWin = true;
+            }
+
+        return foundWin;
+    }
+
+    public static boolean checkDiagonalWin(Player p)
+    {
+        boolean foundWin = true;
+
+        // check top-left to bottom-right
+        for (int i = 0; i < boardSize; i++)
+            if (board[i][i] != p)  // no win found, move on to check other diagonal
+            {
+                foundWin = false;
+                break;
+            }
+
+
+        if (foundWin)
+            return true;
+        else // check bottom-left to top-right
+            for (int i = 0; i < boardSize; i++) {
+                if (board[i][boardSize - 1 - i] != p)
+                    return false; // no win in this diagonal either, return false
+            }
+
+        return true; // found win in bottom-left to top-right diagonal
+
+    }
+
+    public static boolean checkFourCornersWin(Player p)
+    {
+        return board[0][0] == p
+                && board[boardSize-1][0] == p
+                && board[0][boardSize-1] == p
+                && board[boardSize-1][boardSize-1] == p;
+    }
+
+    public static boolean checkSquareWin(Player p)
+    {
+        int l = boardSize/2;
+        boolean foundWin = false;
+
+//        // spiral to the center
+//        for (int col = 0; col < boardSize; col++)
+//            for (int)
+
+        // there aren't too many possibilities for a square win
+        // first, we start by checking each corner
+
+
+        // top-left
+        if (board[0][0] == p)
+            if (checkWinInNeighbors(p, 0, 0, l))
+                return true; // found a win in top-left corner
+
+
+        // bottom-left
+        if (board[0][boardSize-1] == p)
+            if (checkWinInNeighbors(p, 0, boardSize-1, l))
+                return true; // found a win in top-right corner
+
+        // bottom-right
+        if (board[boardSize-1][boardSize-1] == p)
+            if (checkWinInNeighbors(p, boardSize-1, boardSize-1, l))
+                return true; // found a win in bottom-right corner
+
+        // top-right
+        if (board[boardSize-1][0] == p)
+            if (checkWinInNeighbors(p, boardSize-1, 0, l))
+                return true;
+
+        // check middle
+
+        return false;
+    }
+
+    private static boolean checkWinInNeighbors(Player p, int col, int row, int num)
+    {
+        return false; // TODO
+    }
+
+    //endregion
+}
--- app/src/test/java/com/williamgrand/tictactoe/ExampleUnitTest.java
+++ app/src/test/java/com/williamgrand/tictactoe/ExampleUnitTest.java
@@ -8,6 +8,85 @@ import static org.junit.Assert.*;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
+
+    // region win checks
+
+//    @Test
+//    public void check_allWins() throws Exception {
+//
+//
+//
+//
+//        if (checkVerticalWin(p))
+//            return true;
+//        else if (checkHorizontalWin(p))
+//            return true;
+//        else if (checkDiagonalWin(p))
+//            return true;
+//        else if(checkFourCornersWin(p))
+//            return true;
+//        return false;
+//    }
+
+    @Test
+    public void checkVerticalWin(Player p) throws Exception {
+
+        Player[][] winningBoard = new Player[][] {
+                {null, Player.O, Player.X, null},
+                {null, Player.O, Player.X, null},
+                {null, Player.O, Player.X, null},
+                {null, Player.O, Player.X, null}
+        };
+
+        GameManager.board = winningBoard;
+
+        // O
+        if(BuildConfig.DEBUG && !GameManager.checkVerticalWin(Player.O)) throw new AssertionError();
+
+        // X
+        if(BuildConfig.DEBUG && !GameManager.checkVerticalWin(Player.X)) throw new AssertionError();
+
+        Player[][] losingBoard = new Player[][] {
+                {null, null, null, null},
+                {null, Player.O, Player.X, null},
+                {null, Player.O, Player.X, null},
+                {null, Player.O, Player.X, null}
+        };
+
+        GameManager.board = losingBoard;
+
+        // O
+        if (BuildConfig.DEBUG && !GameManager.checkVerticalWin(Player.O)) throw new AssertionError();
+
+        // X
+        if (BuildConfig.DEBUG && !GameManager.checkVerticalWin(Player.X)) throw new AssertionError();
+
+
+    }
+
+//    @Test
+//    public static boolean checkHorizontalWin(Player p)
+//    {
+//    }
+//
+//    @Test
+//    public static boolean checkDiagonalWin(Player p)
+//    {
+//
+//    }
+//
+//    @Test
+//    public static boolean checkFourCornersWin(Player p)
+//    {
+//    }
+//
+//    @Test
+//    public static boolean checkSquareWin(Player p) {
+//
+//    }
+
+
+    // endregion
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
